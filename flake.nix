f2770b190c5974818daaf11def62e6228c3bf3bf
sgt0 <140186177+sgt0@users.noreply.github.com>
2025-08-13 00:46:54 +0000

    Add Linux and macOS CI

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            meson
+            ninja
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            vapoursynth
+          ];
+        };
+
+        formatter = pkgs.alejandra;
+      }
+    );
+}
